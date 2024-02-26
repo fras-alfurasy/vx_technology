da7a69a172d721d3581174f664661a389c710dd6
unknown <frasalfuracy@gmail.com>
2024-02-26 16:38:14 +0300

    feat(vx-proto): add the text reveal card component and adjust with arguments in the index file

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,33 +1,39 @@
 import { Spotlight } from "../../components/ui/Spotlight";
 import { SparklesCore } from "../../components/ui/sparkles";
+import { TextRevealCard } from "../../components/ui/text-reveal-card";
 
 export function SpotlightPreview() {
   return (
-    <div className="h-[40rem] w-full  flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-[50rem] w-full  flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
+        className="-top-40 left-40 md:left-90 md:-top-20"
         fill="purple"
       />
-            <div className="h-[40rem] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-      <div className="w-full absolute inset-0 h-screen">
-        <SparklesCore
-          id="tsparticlesfullpage"
-          background="transparent"
-          minSize={0.6}
-          maxSize={1.4}
-          particleDensity={100}
-          className="w-full h-full"
-          particleColor="#FFFFFF"
-        />
-      </div>
-      <div className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-white relative z-20">
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-purple-700 to-purple-400 bg-opacity-50">
-          VisualX <br /> Technologies.
-        </h1>
-        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">Technologically Blind We Will Make You See</p>
-      </div>
-    </div>
+      <div className="h-[50rem] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+        <div className="w-full absolute inset-0 h-screen">
+          <SparklesCore
+            id="tsparticlesfullpage"
+            background="transparent"
+            minSize={0.6}
+            maxSize={1.4}
+            particleDensity={100}
+            className="w-full h-full"
+            particleColor="#A020F0"
+          />
+        </div>
+        <div className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-white relative z-20">
+          <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+            <h1 className="text-4xl md:text-7xl h-[10rem] font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-purple-700 to-purple-400 bg-opacity-50">
+              VisualX <br /> Technologies.
+            </h1>
+            <div className="ml-[6rem]">
+            <TextRevealCard
+              text="Technologically Blind"
+              revealText="We Will Make You See"
+            />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
